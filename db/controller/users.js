47003b7075ccb28554ad4f300bc5145b21638f67
Gid3onJura <homebasekorlash@googlemail.com>
2022-12-23 17:21:13 +0100

    setup for https connections

--- db/controller/users.js
+++ db/controller/users.js
@@ -1,62 +1,57 @@
-const md5 = require('md5');
-const User = require('../models/User');
+const md5 = require("md5");
+const User = require("../models/User");
 
 module.exports = {
-    findAllUser: async function () {
-        try {
-            const users = await User.findAll({
-                attributes: ['id', 'nickname', 'email', 'dojo', 'password']
-            }).catch((error) => []);
-            return users;
-        }
-        catch (error) {
-            console.log(error);
-            return []
-        }
-    },
-    nicknameExists: async function (nickname) {
-        try {
-            const user = await User.findOne({
-                attributes: ['id'],
-                where: { nickname: nickname }
-            });
-            if (user) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-        catch (error) {
-            console.log(error);
-            return true;
-        }
-    },
-    createUser: async function (data) {
-        try {
-            const newUser = await User.create(data);
-            return true;
-        }
-        catch (error) {
-            console.log(error);
-            return false;
-        }
-
-    },
-    userExists: async function (nickname, password) {
-        try {
-            const user = await User.findOne({
-                attributes: ['id', 'nickname'],
-                where: { nickname: nickname, password: md5(password) }
-            });
-            if (user) {
-                return user.dataValues;
-            } else {
-                return false;
-            }
-        }
-        catch (error) {
-            console.log(error);
-            return false;
-        }
+  findAllUser: async function () {
+    try {
+      const users = await User.findAll({
+        attributes: ["id", "nickname", "email", "dojo", "password"],
+      }).catch((error) => []);
+      return users;
+    } catch (error) {
+      console.log(error);
+      return [];
     }
-}
+  },
+  nicknameExists: async function (nickname) {
+    try {
+      const user = await User.findOne({
+        attributes: ["id"],
+        where: { nickname: nickname },
+      });
+      if (user) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return true;
+    }
+  },
+  createUser: async function (data) {
+    try {
+      const newUser = await User.create(data);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+  userExists: async function (nickname, password) {
+    try {
+      const user = await User.findOne({
+        attributes: ["id", "nickname"],
+        where: { nickname: nickname, password: md5(password), activated: true },
+      });
+      if (user) {
+        return user.dataValues;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+};
--- middleware/resolveApiKey.js
+++ middleware/resolveApiKey.js
@@ -1,9 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
 exports.resolveApiKey = (request, response, next) => {
-    if (request.headers && request.headers['api-key'] && request.headers['api-key'] === process.env.API_KEY) {
-        next()
-    } else {
-        return response.status(403).send();
-    }
-}
+  if (
+    request.headers &&
+    request.headers["api-key"] &&
+    request.headers["api-key"] === process.env.API_KEY
+  ) {
+    next();
+  } else {
+    return response.status(403).send({});
+  }
+};
