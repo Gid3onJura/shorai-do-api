583187e17f1a6c15ea90ff26fd9c3bb51b65893e
Gid3onJura <homebasekorlash@googlemail.com>
2023-07-01 21:45:05 +0200

    update cors options

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const BASE_URL = process.env.BASE_URL || "http://localhost"
 
-app.use(
-  cors({
-    allowedHeaders: [
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Methods",
-      "Access-Control-Allow-Origin",
-      "Content-Type",
-      "Authorization",
-      "api-key",
-    ],
-    origin: "*",
-    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-    preflightContinue: false,
-    credentials: true,
-  })
-)
-//app.use(cors())
+// app.use(
+//   cors({
+//     allowedHeaders: [
+//       "Access-Control-Allow-Headers",
+//       "Access-Control-Allow-Methods",
+//       "Access-Control-Allow-Origin",
+//       "Content-Type",
+//       "Authorization",
+//       "api-key",
+//     ],
+//     origin: "*",
+//     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+//     preflightContinue: false,
+//     credentials: true,
+//   })
+// )
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -33,12 +33,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(resolveApiKey)
 
 // set header to all responses
-app.use(function (request, response, next) {
-  response.setHeader("Content-Type", "application/json")
-  response.setHeader("Access-Control-Allow-Origin", ["*"])
-  response.setHeader("Access-Control-Allow-Methods", ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"])
-  next()
-})
+// app.use(function (request, response, next) {
+//   response.setHeader("Content-Type", "application/json")
+//   response.setHeader("Access-Control-Allow-Origin", ["*"])
+//   response.setHeader("Access-Control-Allow-Methods", ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"])
+//   next()
+// })
 
 //const mockUser = require('./mock/user.json');
 
