a11778d231912a3468283f59f08aeb51686c9551
Gid3onJura <homebasekorlash@googlemail.com>
2023-07-06 00:00:38 +0200

    update body validation

--- validation/validation.js
+++ validation/validation.js
@@ -1,24 +1,24 @@
-const Joi = require("joi");
+const Joi = require("joi")
 
 const validation = (schema, property) => {
   return (request, response, next) => {
     const options = {
       abortEarly: false, // include all errors
-      allowUnknown: true, // ignore unknown props
-      stripUnknown: true, // remove unknown props
-    };
-    const { error } = schema.validate(request[property], options);
-    const valid = error == null;
+      allowUnknown: false, // ignore unknown props
+      stripUnknown: false, // remove unknown props
+    }
+    const { error } = schema.validate(request[property], options)
+    const valid = error == null
 
     if (valid) {
-      next();
+      next()
     } else {
-      const { details } = error;
-      const message = details.map((i) => i.message).join(",");
+      const { details } = error
+      const message = details.map((i) => i.message).join(",")
 
-      console.log("error", message);
-      return response.status(400).json({ error: message });
+      console.log("error", message)
+      return response.status(400).json({ error: message })
     }
-  };
-};
-module.exports = validation;
+  }
+}
+module.exports = validation
