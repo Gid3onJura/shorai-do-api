c7c15a1c2bc3ff5b41ace9f0af227ed601c25be9
Gid3onJura <homebasekorlash@googlemail.com>
2023-07-23 17:44:33 +0200

    set cors

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const BASE_URL = process.env.BASE_URL || "http://localhost"
 
-// app.use(
-//   cors({
-//     allowedHeaders: [
-//       "Access-Control-Allow-Headers",
-//       "Access-Control-Allow-Methods",
-//       "Access-Control-Allow-Origin",
-//       "Content-Type",
-//       "Authorization",
-//       "api-key",
-//     ],
-//     origin: "*",
-//     methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
-//     preflightContinue: false,
-//     credentials: true,
-//   })
-// )
-app.use(cors())
+app.use(
+  cors({
+    allowedHeaders: [
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Methods",
+      "Access-Control-Allow-Origin",
+      "Content-Type",
+      "Authorization",
+      "api-key",
+    ],
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
+    preflightContinue: false,
+    credentials: true,
+  })
+)
+// app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
