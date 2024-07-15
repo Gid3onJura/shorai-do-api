3310c2da85c447d085ac766f316db4d17070c34b
Gid3onJura <homebasekorlash@googlemail.com>
2024-07-16 00:26:03 +0200

    clean up sql statements

--- db/data/shorai-do.sql
+++ db/data/shorai-do.sql
@@ -1,27 +1,6 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
---
--- Host: localhost    Database: shorai-do
--- ------------------------------------------------------
--- Server version	5.5.5-10.4.25-MariaDB
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `dojos`
 --
-
-DROP TABLE IF EXISTS `dojos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `dojos` (
   `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
@@ -32,10 +11,6 @@ CREATE TABLE `dojos` (
 --
 -- Table structure for table `ranks`
 --
-
-DROP TABLE IF EXISTS `ranks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ranks` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `rank` tinyint(3) unsigned NOT NULL,
@@ -53,10 +28,6 @@ CREATE TABLE `ranks` (
 --
 -- Table structure for table `users`
 --
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
   `nickname` varchar(100) NULL,
@@ -92,13 +63,13 @@ CREATE TABLE `events` (
   `eventcolor` varchar(100) DEFAULT NULL,
   `description` varchar(100) NOT NULL,
   `override` tinyint(1) DEFAULT 0,
-  `createdat` datetime NOT NULL DEFAULT current_timestamp(),
-  `updatedat` datetime DEFAULT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL,
   `repeating` tinyint(1) DEFAULT 0,
   `eventtype` varchar(100) NOT NULL DEFAULT 'training',
   `repetitiontype` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Table structure for table `refreshTokens`
